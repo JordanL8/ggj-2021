4b34ca2d190259b2887a3608d50787321f2c25dc
Katie Bliben (blibenk) <blibenk@coventry.ac.uk>
2021-01-31 00:09:27 +0000

    break obj task

--- Animal Rangers/Assets/Scripts/Minigame/BreakObject.cs
+++ Animal Rangers/Assets/Scripts/Minigame/BreakObject.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+
 using UnityEngine;
 
 public class BreakObject : MiniGame
 {
     private int breakHits = 5;
-    public GameObject[] breakPoints; 
-
+    public Button[] breakPoints;
+    private Button temp;
     public void breakObj()
     {
         breakHits -= 1;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        breakHits = Random.Range(1, 6);
-       
+        breakHits = Random.Range(2, 6);
+        Debug.Log("hits" + breakHits);
+
+
+        int rnd = Random.Range(1, breakPoints.Length);
+         
+        for (int i = 0; i < breakHits; i++)
+        {
+            int randomIndex = Random.Range(i, breakPoints.Length);
+            temp = breakPoints[randomIndex];
+            breakPoints[randomIndex] = breakPoints[i];
+            breakPoints[i] = temp;
+            breakPoints[i].gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
