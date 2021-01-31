27395cca8739d16e1fcc7c8cedcdfb1e767d0247
Joanna Wilshire (wilshirj) <wilshirj@coventry.ac.uk>
2021-01-31 13:33:31 +0000

    Waypoints and Camera

--- Animal Rangers/Assets/Scripts/Minigame/Line Game/PathClick.cs
+++ Animal Rangers/Assets/Scripts/Minigame/Line Game/PathClick.cs
@@ -20,12 +20,19 @@ public class PathClick : SingleSceneSingleton<PathClick>
         // Failed
         if (isGood && (!mouseDown || !inLine))
         {
-            GetComponent<SpriteRenderer>().color = Color.Lerp(Color.red, Color.white, 0.005f);
+            GetComponent<SpriteRenderer>().color = Color.red;
+            LeanTween.value(gameObject, Color.red, Color.white, 1.5f).setOnUpdateColor(UpdateColour);
+
             mouseDown = false;
             inLine = false;
             startClicked = false;
             isGood = false;
         }
+    }
+
+    void UpdateColour(Color colour)
+    {
+        GetComponent<SpriteRenderer>().color = colour;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
